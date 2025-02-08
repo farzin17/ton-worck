10e597b2170bf36f98299eec18dec0189a016442
farzin heydari <farzinheydari095@gmail.com>
2025-02-08 22:17:49 +0330

    actor model

---
+++ script.js
@@ -0,0 +1,49 @@
+let buttom = document.querySelector(".click1");
+buttom.addEventListener("click", onsubmit)
+function onsubmit(){
+    let input_el = document.querySelector(".one");
+    let value = input_el.value;
+    console.log(value)
+}
+let buttom_2 = document.querySelector(".click2");
+buttom_2.addEventListener("click", onsubmit)
+function onsubmit(){
+    let input_el = document.querySelector(".one");
+    let value = input_el.value;
+    console.log(value)
+}
+let buttom_3 = document.querySelector(".click3");
+buttom_3.addEventListener("click", onsubmit)
+function onsubmit(){
+    let input_el = document.querySelector(".one");
+    let value = input_el.value;
+    console.log(value)
+}
+let buttom_4 = document.querySelector(".click4");
+buttom_4.addEventListener("click", onsubmit)
+function onsubmit(){
+    let input_el = document.querySelector(".one");
+    let value = input_el.value;
+    console.log(value)
+}
+let buttom_5 = document.querySelector(".click5");
+buttom_5.addEventListener("click", onsubmit)
+function onsubmit(){
+    let input_el = document.querySelector(".one");
+    let value = input_el.value;
+    console.log(value)
+}
+let buttom_6 = document.querySelector(".click6");
+buttom_6.addEventListener("click", onsubmit)
+function onsubmit(){
+    let input_el = document.querySelector(".one");
+    let value = input_el.value;
+    console.log(value)
+}
+let buttom_7 = document.querySelector(".click7");
+buttom_7.addEventListener("click", onsubmit)
+function onsubmit(){
+    let input_el = document.querySelector(".one");
+    let value = input_el.value;
+    console.log(value)
+}
